519592b3a4517c742237587534daa9ea88ac3853
wisnuajipamungkas354 <wisnuajipamungkas354@gmail.com>
2023-09-25 16:53:30 +0700

    mengubah default controller, menambahkan file latihan1.php

---
+++ pustaka-booking/application/controllers/latihan1.php
@@ -0,0 +1,19 @@
+<?php
+class Latihan1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "
+    <h3>Biodata Mahasiswa UBSI Cikarang :</h3>    
+    <ul>
+    <li>Nama : Wisnu Aji Pamungkas</li>
+    <li>NIM : 19221106</li>
+    <li>Kelas : 19.3A.24 SI.S1</li>
+    <li>Umur : 21 Tahun</li>
+    <li>Alamat : Karawang</li>
+    <li>No. HP : 085889634432</li>
+    <li>Hobby : Ngoprek Laptop</li>
+    </ul>
+    ";
+    }
+}
