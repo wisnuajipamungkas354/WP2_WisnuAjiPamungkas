7c63f9bcbe0bde66d5640add67a99c362b9eef04
wisnuajipamungkas354 <wisnuajipamungkas354@gmail.com>
2023-10-02 16:46:28 +0700

    Mengerjakan contoh3, mengubah file latihan2.php dan menambahkan file view-latihan2.php

--- pustaka-booking/application/controllers/latihan2.php
+++ pustaka-booking/application/controllers/latihan2.php
@@ -9,7 +9,11 @@ class Latihan2 extends CI_Controller
     public function penjumlahan($n1, $n2)
     {
         $this->load->model('Model_latihan2');
-        $hasil = $this->Model_latihan2->jumlah($n1, $n2);
-        echo "Hasil Penjumlahan dari " . $n1 . " + " . $n2 . " = " . $hasil;
+
+        $data['nilai1'] = $n1;
+        $data['nilai2'] = $n1;
+        $data['hasil'] = $this->Model_latihan2->jumlah($n1, $n2);
+
+        $this->load->view('view-latihan2', $data);
     }
 }
