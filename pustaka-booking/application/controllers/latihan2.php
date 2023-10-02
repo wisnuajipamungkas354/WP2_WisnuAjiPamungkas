15b41856f7a23f7c21b8f449c5d0cfddb3afff0d
wisnuajipamungkas354 <wisnuajipamungkas354@gmail.com>
2023-10-02 16:40:59 +0700

    Mengerjakan Contoh2, menambahkan controller penjumlahan dan file model_latihan2.php

--- pustaka-booking/application/controllers/latihan2.php
+++ pustaka-booking/application/controllers/latihan2.php
@@ -5,4 +5,11 @@ class Latihan2 extends CI_Controller
     {
         echo "Selamat Datang... Selamat Belajar Web Programming";
     }
+
+    public function penjumlahan($n1, $n2)
+    {
+        $this->load->model('Model_latihan2');
+        $hasil = $this->Model_latihan2->jumlah($n1, $n2);
+        echo "Hasil Penjumlahan dari " . $n1 . " + " . $n2 . " = " . $hasil;
+    }
 }
